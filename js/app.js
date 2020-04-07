6ed52ca8742292421c85e872d562b235ede03f75
Habib Afolabi <habibwritescode@gmail.com>
2020-04-07 02:08:22 +0100

    feature: Add active styling to nav links

--- js/app.js
+++ js/app.js
@@ -18,6 +18,9 @@
  * 
  */
 
+const subHeadings = document.querySelectorAll('h2');
+const navUl = document.querySelector('#navbar__list');
+const sections = document.querySelectorAll('section');
 
 /**
  * End Global Variables
@@ -26,34 +29,7 @@
  */
 
 
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
- */
-
-// build the nav
-const fragment = document.createDocumentFragment();
-const subHeadings = document.querySelectorAll('h2');
-const navUl = document.querySelector('#navbar__list')
-const sections = document.querySelectorAll('section')
-const links = navUl.getElementsByClassName('links')
-
-for (let i = 0; i < subHeadings.length; i++) {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `<a>${subHeadings[i].textContent}</a>`;
-    listItem.querySelector('a').setAttribute('href', '#' + sections[i].id);
-    listItem.querySelector('a').className = 'link'
-
-    fragment.appendChild(listItem);
-}
-
-navUl.appendChild(fragment);
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
+// Listener for scrolling into view when navigation is clicked
 function respondToClick(evt) {
     if (evt.target.nodeName === 'A') {
         evt.preventDefault();
@@ -64,7 +40,54 @@ function respondToClick(evt) {
     }
 }
 
-navUl.addEventListener('click', respondToClick);
+
+// Listener for knowing if a section is in the viewport
+function isIntoView(element) {
+    let rect = element.getBoundingClientRect();
+    let elemTop = rect.top;
+    let elemBottom = rect.bottom;
+
+    // Visible elements will return true:
+    let isInView = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    return isInView;
+}
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+ */
+
+
+// build the nav
+function buildNav() {
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 0; i < subHeadings.length; i++) {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `<a>${subHeadings[i].textContent}</a>`;
+        listItem.querySelector('a').setAttribute('href', '#' + sections[i].id);
+        listItem.querySelector('a').className = 'link';
+
+        fragment.appendChild(listItem);
+    }
+
+    return navUl.appendChild(fragment);
+}
+buildNav()
+
+
+// Add class 'active' to section when near top of viewport
+
+
+// Scroll to anchor ID using scrollTO event
+
+function scrollToAnchorId() {
+    return navUl.addEventListener('click', respondToClick);
+}
+scrollToAnchorId()
+
 
 /**
  * End Main Functions
@@ -77,20 +100,10 @@ navUl.addEventListener('click', respondToClick);
 // Scroll to section on link click
 
 // Set sections as active
-function isScrolledIntoView(el) {
-    var rect = el.getBoundingClientRect();
-    var elemTop = rect.top;
-    var elemBottom = rect.bottom;
 
-    // Only completely visible elements return true:
-    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
-    return isVisible;
-}
-
-
-window.addEventListener('scroll', function(event) {
+window.addEventListener('scroll', function() {
     for (let i = 0; i < sections.length; i++) {
-        if (isScrolledIntoView(sections[i])) {
+        if (isIntoView(sections[i])) {
             sections[i].classList.add('your-active-class');
         } else {
             sections[i].classList = "";
@@ -98,3 +111,24 @@ window.addEventListener('scroll', function(event) {
         }
     }
 })
+
+
+// Loop through the links and add the active class to the current/clicked link
+function addActiveClassToLink() {
+    const links = navUl.getElementsByClassName('link')
+
+    for (var i = 0; i < links.length; i++) {
+        links[i].addEventListener("click", function() {
+            var current = document.getElementsByClassName("active");
+
+            // If there's no active class
+            if (current.length > 0) {
+                current[0].className = current[0].className.replace(" active", "");
+            }
+
+            // Add the active class to the current/clicked link
+            this.className += " active";
+        });
+    }
+}
+addActiveClassToLink()
